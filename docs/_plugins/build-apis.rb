be4e639f171df1001b05f25a6043995fd8f9e11a
Alex Bozarth <ajbozart@us.ibm.com>
2018-01-17 16:22:32 -0800

    Updated javadoc build and included api doc build in livy doc build

---
+++ docs/_plugins/build-apis.rb
@@ -0,0 +1,57 @@
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require 'fileutils'
+include FileUtils
+
+if not (ENV['SKIP_API'] == '1')
+  # Build Scaladoc for Scala and Javadoc for Java
+
+  puts "Moving to project root and building API docs."
+  curr_dir = pwd
+  cd("..")
+
+  puts "Running 'mvn package scala:doc -DskipTests -DskipITs javadoc:javadoc -Ppublic-docs' from " + pwd + "; this may take a few minutes..."
+  system("mvn package scala:doc -DskipTests -DskipITs javadoc:javadoc -Ppublic-docs") || raise("Maven build failed")
+
+  puts "Moving back into docs dir."
+  cd("docs")
+
+  puts "Removing old docs"
+  puts `rm -rf api`
+
+  # Copy over the ScalaDoc for the Scala API to api/scala.
+  # This directory will be copied over to _site when `jekyll` command is run.
+  source = "../scala-api/target/site/scaladocs"
+  dest = "api/scala"
+
+  puts "Making directory " + dest
+  mkdir_p dest
+
+  # From the rubydoc: cp_r('src', 'dest') makes src/dest, but this doesn't.
+  puts "cp -r " + source + "/. " + dest
+  cp_r(source + "/.", dest)
+
+  # Copy over the JavaDoc for the Java API to api/java.
+  source = "../api/target/site/apidocs"
+  dest = "api/java"
+
+  puts "Making directory " + dest
+  mkdir_p dest
+
+  puts "cp -r " + source + "/. " + dest
+  cp_r(source + "/.", dest)
+
+end
