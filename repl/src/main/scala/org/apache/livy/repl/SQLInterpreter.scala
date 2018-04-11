7e4bb3bd6888742e43aed7396ed490d343a063a2
jerryshao <sshao@hortonworks.com>
2018-04-11 10:24:20 +0800

    [LIVY-455][REPL] Fix json4s doesn't support java.math.BigDecimal issue
    ## What changes were proposed in this pull request?
    
    Livy's SQLInterpreter will throw exception when rows contain java.math.BigDecimal data. This is because current version of json4s doesn't treat java.math.BigDecimal type as primitive type. On the contrary, json4s supports Scala BigDecimal as primitive type. So the fix is to convert java BigDecimal to Scala BigDecimal.
    
    ## How was this patch tested?
    
    Unit test is added.
    
    Author: jerryshao <sshao@hortonworks.com>
    
    Closes #85 from jerryshao/LIVY-455.

--- repl/src/main/scala/org/apache/livy/repl/SQLInterpreter.scala
+++ repl/src/main/scala/org/apache/livy/repl/SQLInterpreter.scala
@@ -97,7 +97,15 @@ class SQLInterpreter(
       val rows = result.getClass.getMethod("take", classOf[Int])
         .invoke(result, maxResult: java.lang.Integer)
         .asInstanceOf[Array[Row]]
-          .map(_.toSeq)
+        .map {
+          _.toSeq.map {
+            // Convert java BigDecimal type to Scala BigDecimal, because current version of
+            // Json4s doesn't support java BigDecimal as a primitive type (LIVY-455).
+            case i: java.math.BigDecimal => BigDecimal(i)
+            case e => e
+          }
+        }
+
       val jRows = Extraction.decompose(rows)
 
       Interpreter.ExecuteSuccess(
