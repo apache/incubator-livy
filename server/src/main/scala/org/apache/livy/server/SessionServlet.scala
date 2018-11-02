bea35264e0a2a394cffabd0830d9e4d61105f312
ingo <ingo.schuster@de.ibm.com>
2018-11-02 15:19:54 +0100

    Add ResponseMessage class to avoid explicit Map constructor in SessionServlet

--- server/src/main/scala/org/apache/livy/server/SessionServlet.scala
+++ server/src/main/scala/org/apache/livy/server/SessionServlet.scala
@@ -112,10 +112,10 @@ abstract class SessionServlet[S <: Session, R <: RecoveryMetadata](
       sessionManager.delete(session.id) match {
         case Some(future) =>
           Await.ready(future, Duration.Inf)
-          Ok(Map("msg" -> "deleted"))
+          Ok(ResponseMessage("deleted"))
 
         case None =>
-          NotFound(Map("msg" -> s"Session ${session.id} already stopped."))
+          NotFound(ResponseMessage(s"Session ${session.id} already stopped."))
       }
     }
   }
@@ -128,7 +128,7 @@ abstract class SessionServlet[S <: Session, R <: RecoveryMetadata](
 
   post("/") {
     if (tooManySessions) {
-      BadRequest(Map("msg" -> "Rejected, too many sessions are being created!"))
+      BadRequest(ResponseMessage("Rejected, too many sessions are being created!"))
     } else {
       val session = sessionManager.register(createSession(request))
       // Because it may take some time to establish the session, update the last activity
@@ -149,8 +149,8 @@ abstract class SessionServlet[S <: Session, R <: RecoveryMetadata](
   }
 
   error {
-    case e: IllegalArgumentException => BadRequest(Map("msg" -> e.getMessage))
-    case e: AccessControlException => Forbidden(Map("msg" -> e.getMessage))
+    case e: IllegalArgumentException => BadRequest(ResponseMessage(e.getMessage))
+    case e: AccessControlException => Forbidden(ResponseMessage(e.getMessage))
   }
 
   /**
@@ -191,7 +191,7 @@ abstract class SessionServlet[S <: Session, R <: RecoveryMetadata](
           Forbidden()
         }
       case None =>
-        NotFound(Map("msg" -> s"Session '$sessionId' not found."))
+        NotFound(ResponseMessage(s"Session '$sessionId' not found."))
     }
   }
 
