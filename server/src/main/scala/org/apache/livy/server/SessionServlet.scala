3ece8fb7dc2dd5e904bdc619701aa92511ad6f13
alexanghh <alexanghh@gmail.com>
2021-12-02 17:45:10 +0800

    withViewAccessSession for access from /sessionId
    Change withUnprotectedSession to withViewAccessSession for get("/:id") and get("/:id/state") to check if the requester is session owner or has rights to view session before returning the session.

--- server/src/main/scala/org/apache/livy/server/SessionServlet.scala
+++ server/src/main/scala/org/apache/livy/server/SessionServlet.scala
@@ -85,13 +85,13 @@ abstract class SessionServlet[S <: Session, R <: RecoveryMetadata](
   }
 
   val getSession = get("/:id") {
-    withUnprotectedSession { session =>
+    withViewAccessSession { session =>
       clientSessionView(session, request)
     }
   }
 
   get("/:id/state") {
-    withUnprotectedSession { session =>
+    withViewAccessSession { session =>
       Map("id" -> session.id, "state" -> session.state.toString)
     }
   }
