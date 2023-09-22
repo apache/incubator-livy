7ef7c2d2876e88c8256ab824667c4c50186d99da
Damon P. Cortesi <d.lifehacker@gmail.com>
2023-09-22 15:23:22 -0700

    Preparing development version 0.8.1-incubating-SNAPSHOT

--- python-api/setup.py
+++ python-api/setup.py
@@ -40,7 +40,7 @@ requirements = [
 
 setup(
     name='livy-python-api',
-    version="0.8.0-incubating",
+    version="0.8.1-incubating-SNAPSHOT",
     packages=["livy", "livy-tests"],
     package_dir={
         "": "src/main/python",
