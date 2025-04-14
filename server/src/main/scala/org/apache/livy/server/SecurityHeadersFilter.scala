f580e71b5805c0b2ed2b93005fce9bc6396354e7
nileshrathi345 <113672051+nileshrathi345@users.noreply.github.com>
2025-04-14 19:25:39 +0530

    [LIVY-785] Enabled adding security related HTTP headers
    ## What changes were proposed in this pull request?
    
    This change introduces a new configuration option `livy.server.security-headers.enabled`.
    When this property is set to true, the following security headers are added to HTTP
    responses by default:
    
     * X-XSS-Protection
     * X-Frame-Options
     * X-Content-Type-Options
     * Strict-Transport-Security
     * Content-Security-Policy
    
    Also, adds content type information to all responses as required when using content type option nosniff
    
    ## How was this patch tested?
    
    Tested manually

---
+++ server/src/main/scala/org/apache/livy/server/SecurityHeadersFilter.scala
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.livy.server
+
+import javax.servlet.{Filter, FilterChain, FilterConfig, ServletRequest, ServletResponse}
+import javax.servlet.http.HttpServletResponse
+
+import org.apache.livy.LivyConf
+
+/**
+ * Adds security related headers to HTTP responses.
+ */
+class SecurityHeadersFilter(livyConf: LivyConf) extends Filter {
+
+  private def isSslEnabled: Boolean = {
+    Option(livyConf.get(LivyConf.SSL_KEYSTORE)).exists(_.length > 0)
+  }
+
+  val headers : Map[String, String] = Map(
+    "X-Content-Type-Options" -> livyConf.get(LivyConf.SECURITY_HEADERS_CONTENT_TYPE_OPTIONS),
+    "X-Frame-Options" -> livyConf.get(LivyConf.SECURITY_HEADERS_FRAME_OPTIONS),
+    "X-XSS-Protection" -> livyConf.get(LivyConf.SECURITY_HEADERS_XSS_PROTECTION),
+    "Strict-Transport-Security" ->
+      (if (isSslEnabled) livyConf.get(LivyConf.SECURITY_HEADERS_STRICT_TRANSPORT_SECURITY) else ""),
+    "Content-Security-Policy" -> livyConf.get(LivyConf.SECURITY_HEADERS_CONTENT_SECURITY_POLICY))
+    .filter(e => e._2 != null && e._2.trim().length > 0)
+
+  override def init(filterConfig: FilterConfig): Unit = {}
+
+  override def doFilter(request: ServletRequest,
+                        response: ServletResponse,
+                        chain: FilterChain): Unit = {
+    val servletResponse = response.asInstanceOf[HttpServletResponse]
+    for ((k, v) <- headers) {
+      servletResponse.addHeader(k, v)
+    }
+    chain.doFilter(request, response)
+  }
+
+  override def destroy(): Unit = {}
+}
